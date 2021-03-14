43846e119a748f1557416c5c04d25be7d03c46e2
Gabr1el94 <bielsystem64@gmail.com>
2021-03-14 15:11:11 -0300

    cadastro usuário implementado

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import {Router} from 'express';
 
+import UserController from './controllers/UserController'
+
 const router = Router();
 
-export default router;
+router.post('/user',UserController.store);
 
+export default router;
